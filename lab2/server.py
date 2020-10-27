8a8c7ef0c38eb9863ab50fb7dc15e48d306b8607
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-10-27 15:00:02 +0100

    Added logic with implementation for endpoint '/api/check/<string>'

--- lab2/server.py
+++ lab2/server.py
@@ -8,6 +8,16 @@ def chekcOutput(input):
     upperCase = False
     digit = False
     specialSign = False
+    
+    for i in input:
+        if i >= 'A' and i <= 'Z':
+            upperCase = True
+        elif i >= 'a' and i <= 'z':
+            lowerCase = True
+        elif i >= '0' and i <= '9':
+            digit = True
+        else:
+            specialSign = True
 
     return jsonify({'lowerCase': lowerCase,
                     'upperCase': upperCase,
