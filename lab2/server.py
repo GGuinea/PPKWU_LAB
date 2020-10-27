f30658e1016df5abb2667029646938d6a610c1d0
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-10-27 14:29:55 +0100

    Added route for String Api

--- lab2/server.py
+++ lab2/server.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/api/check/<input>', methods=['GET'])
+def chekcOutput(input):
+    lowerLetter, upperLetter, specialSign = false
 
 if __name__ == '__main__':
     app.run(port=25000)
