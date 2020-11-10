f0fe1e101302950ef0960aae372162565c5529c4
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-10 15:51:20 +0100

    Added endpoint ad example of file

--- lab3/server.py
+++ lab3/server.py
@@ -5,28 +5,32 @@ import requests
 import urllib3
 import urllib
 
-page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10")
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
-days = soup.find_all('a')
-day = 2
-lastDay = len(days)-1
 
-events = soup.find_all('a', class_='active')
-print(events[0]['href'])
+app = Flask(__name__)
 
-desc = soup.find_all(class_='InnerBox')
-print(desc[0].getText())
+@app.route('/api/check/<string:year>/<sting:month>')
+def checkOutput(year, month):
+    page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok="+year+"&miesiac="+month)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-c = Calendar()
-for i in range(len(events)):
-    e = Event()
-    e.name = desc[i].getText()
-    if int(events[i].getText()) < 10:
-        e.begin = '2020-10-0' + events[i].getText() + ' 00:00:00'
-    else:
-        e.begin = '2020-10-' + events[i].getText() + ' 00:00:00'
-    c.events.add(e)
+    events = soup.find_all('a', class_='active')
+    print(events[0]['href'])
 
-with open('my.ics', 'w') as my_file:
-    my_file.writelines(c)
+    desc = soup.find_all(class_='InnerBox')
+    print(desc[0].getText())
+
+    c = Calendar()
+    for i in range(len(events)):
+        e = Event()
+        e.name = desc[i].getText()
+        if int(events[i].getText()) < 10:
+            e.begin = '2020-10-0' + events[i].getText() + ' 00:00:00'
+        else:
+            e.begin = '2020-10-' + events[i].getText() + ' 00:00:00'
+        c.events.add(e)
+
+    with open('my.ics', 'w') as my_file:
+        my_file.writelines(c)
+
+if __name__ == '__main__':
+    app.run(port=25000)
