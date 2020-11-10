cdfcf1b76546a2f8f4765920b922ba11060c8eec
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-10 15:21:46 +0100

    Printed days

--- lab3/server.py
+++ lab3/server.py
@@ -4,8 +4,10 @@ import requests
 import urllib3
 import urllib
 
-page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php")
-print(page)
-print(page.content)
+page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=month")
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+days = soup.find_all('a')
+day = 2
+while day <= len(days)-1:
+    print(days[day].getText())
+    day += 1
