120493c746488b9e688bcc365dda7940707b4089
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-10 15:35:07 +0100

    Reading href from page

--- lab3/server.py
+++ lab3/server.py
@@ -4,10 +4,14 @@ import requests
 import urllib3
 import urllib
 
-page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=month")
+page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10")
 soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
 days = soup.find_all('a')
 day = 2
 while day <= len(days)-1:
     print(days[day].getText())
     day += 1
+
+events = soup.find_all('a', class_='active')
+print(events[0]['href'])
