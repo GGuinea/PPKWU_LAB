7bc386f421b667f242da8935c00060caf836dcbc
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-10 15:02:46 +0100

    Prettifing output

--- lab3/server.py
+++ lab3/server.py
@@ -7,3 +7,5 @@ import urllib
 page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php")
 print(page)
 print(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
