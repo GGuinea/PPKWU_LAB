ecc3a0749e2ffdba0b12217acdbaad9fc5ac9acc
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-10 15:01:00 +0100

    Reading page from server

--- lab3/server.py
+++ lab3/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
+from bs4 import BeautifulSoup
+import requests
+import urllib3
+import urllib
 
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(port=25000)
+page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php")
+print(page)
+print(page.content)
