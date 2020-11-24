ae4383a260703da094203bca24369218cf1f9e22
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-11-24 14:17:41 +0100

    Fixed way of presenting values

--- lab3/server.py
+++ lab3/server.py
@@ -8,8 +8,11 @@ import urllib
 
 app = Flask(__name__)
 app.config["calendar"] = "ics"
-@app.route('/api/check/<string:year>/<sting:month>')
+@app.route('/api/check/<string:year>/<string:month>')
 def checkOutput(year, month):
+    if int(month) < 10:
+        month = '0' + month
+
     page = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok="+year+"&miesiac="+month)
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -24,9 +27,9 @@ def checkOutput(year, month):
         e = Event()
         e.name = desc[i].getText()
         if int(events[i].getText()) < 10:
-            e.begin = '2020-10-0' + events[i].getText() + ' 00:00:00'
+            e.begin = year + '-' + month + '-0' + events[i].getText() + ' 00:00:00'
         else:
-            e.begin = '2020-10-' + events[i].getText() + ' 00:00:00'
+            e.begin = year + '-' + month + '-' + events[i].getText() + ' 00:00:00'
         c.events.add(e)
 
     with open('my.ics', 'w') as my_file:
