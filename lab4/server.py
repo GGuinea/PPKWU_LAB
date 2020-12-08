9fab729008797262b04eab5ca96b65a996610639
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-12-08 15:20:50 +0100

    Parsing to object

--- lab4/server.py
+++ lab4/server.py
@@ -6,13 +6,33 @@ import urllib3
 import json
 import urllib
 
-def checkOutput(name):
+class Company:
+    name = ""
+    telephone = ""
+    email = ""
+    url = ""
+    address = {}
 
+    def __init__(self, name, telephone, email, url, address):
+        self.name = name
+        self.telephone = telephone
+        self.email = email
+        self.url = url
+        self.address = address
+
+companies = []
+def checkOutput(name):
     page = requests.get("https://panoramafirm.pl/szukaj?k="+name+"&l=")
     soup = BeautifulSoup(page.content, 'html.parser')
     all_data = soup.find_all('script', type='application/ld+json')
+    i = 0
     for script in all_data:
+        if i == (len(all_data) -1):
+            break
+        i+=1
         jsonn = json.loads(script.string)
-        print(jsonn['name'])
+        companies.append(Company(jsonn['name'], jsonn['telephone'], jsonn['email'], jsonn['url'], jsonn['address']))
 
 checkOutput('Hydraulik')
+print(companies[24].name)
+
