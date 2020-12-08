0d1987336931b23f4f0ec003d6b30ddd6a03cb2a
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-12-08 15:02:50 +0100

    Parsed content with output from page

--- lab4/server.py
+++ lab4/server.py
@@ -3,13 +3,23 @@ from bs4 import BeautifulSoup
 from ics import Calendar, Event
 import requests
 import urllib3
+import json
 import urllib
 
 def checkOutput(name):
 
     page = requests.get("https://panoramafirm.pl/szukaj?k="+name+"&l=")
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup)
+    data = soup.find_all('script')
+    #3 - 28
+    #print(data[27].contents)
+    #names = [el['name'] for el in json.loads(data[27].contents.string)['itemListElement']]
+    #print(names)
+    s = soup.find('script', type='application/ld+json')
 
+    jsonn = json.loads(s.string)
+    print(jsonn['name'])
+
+    #k,wprint(soup.find_all("disable"))
 
 checkOutput('Hydraulik')
