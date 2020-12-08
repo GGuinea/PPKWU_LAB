474d2617bf2e6f42a242e18a426c00d831f4f427
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-12-08 15:07:36 +0100

    Cleaned code

--- lab4/server.py
+++ lab4/server.py
@@ -10,16 +10,9 @@ def checkOutput(name):
 
     page = requests.get("https://panoramafirm.pl/szukaj?k="+name+"&l=")
     soup = BeautifulSoup(page.content, 'html.parser')
-    data = soup.find_all('script')
-    #3 - 28
-    #print(data[27].contents)
-    #names = [el['name'] for el in json.loads(data[27].contents.string)['itemListElement']]
-    #print(names)
-    s = soup.find('script', type='application/ld+json')
-
-    jsonn = json.loads(s.string)
-    print(jsonn['name'])
-
-    #k,wprint(soup.find_all("disable"))
+    all_data = soup.find_all('script', type='application/ld+json')
+    for script in all_data:
+        jsonn = json.loads(script.string)
+        print(jsonn['name'])
 
 checkOutput('Hydraulik')
