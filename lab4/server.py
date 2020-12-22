61b2dc7aa9cae4cf01d9f4140df075318a8428a0
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-12-22 15:00:30 +0100

    Added api

--- lab4/server.py
+++ lab4/server.py
@@ -7,6 +7,8 @@ import json
 import urllib
 import vobject
 
+app = Flask(__name__)
+
 class Company:
     name = ""
     telephone = ""
@@ -22,17 +24,6 @@ class Company:
         self.address = address
 
 companies = []
-def checkOutput(name):
-    page = requests.get("https://panoramafirm.pl/szukaj?k="+name+"&l=")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    all_data = soup.find_all('script', type='application/ld+json')
-    i = 0
-    for script in all_data:
-        if i == (len(all_data) -1):
-            break
-        i+=1
-        jsonn = json.loads(script.string)
-        companies.append(Company(jsonn['name'], jsonn['telephone'], jsonn['email'], jsonn['url'], jsonn['address']))
 
 def create_vCard(company):
     card = vobject.vCard()
@@ -50,18 +41,36 @@ def create_vCard(company):
     return card
 
 
-checkOutput('Hydraulik')
-vcards = []
-for company in companies:
-    vcards.append(create_vCard(company))
+#checkOutput('Hydraulik')
+def save_Vcards():
+    vcards = []
+    for company in companies:
+        vcards.append(create_vCard(company))
 
-i = 0
-for card in vcards:
-    print("NAME:",card.n.value)
-    print(card.serialize())
-    filename = "vcf/company"+str(i)+".vcf"
-    i+=1
-    with open (filename, 'w') as my_file:
-        my_file.writelines(card.serialize())
+    i = 0
+    for card in vcards:
+        print("NAME:",card.n.value)
+        print(card.serialize())
+        filename = "vcf/company"+str(i)+".vcf"
+        i+=1
+        with open (filename, 'w') as my_file:
+            my_file.writelines(card.serialize())
+
+@app.route('api/vcard/<string:name>')
+def checkOutput(name):
+    page = requests.get("https://panoramafirm.pl/szukaj?k="+name+"&l=")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    all_data = soup.find_all('script', type='application/ld+json')
+    i = 0
+    for script in all_data:
+        if i == (len(all_data) -1):
+            break
+        i+=1
+        jsonn = json.loads(script.string)
+        companies.append(Company(jsonn['name'], jsonn['telephone'], jsonn['email'], jsonn['url'], jsonn['address']))
+    save_Vcards()
 
 
+
+if __name__ == '__main__':
+    app.run(port=25000)
